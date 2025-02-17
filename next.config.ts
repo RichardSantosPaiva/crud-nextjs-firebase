2eb12f65f07e7e726cf5ef56d59b5cffbce5d233
RichardSantosPaiva <richardpaiva0101@gmail.com>
2025-02-17 18:32:49 -0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false, // Pode evitar problemas com URLs na Vercel
+  output: "standalone", // Garante que o build funcione corretamente na Vercel
 };
 
 export default nextConfig;
